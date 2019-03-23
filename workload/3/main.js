8cffa4766d6cd5ac0f43b8256558ae06f81b4e6c
RobinKohrs <robin.kohrs@gmx.de>
2019-03-23 21:00:16 +0100

    7/9 static checking

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,6 +1,11 @@
-public class Song {
-	public static void main(String[] args) {   
-String openingLyrics= "Yesterday, all my troubles seemed so far away";
-    System.out.println(openingLyrics);
-	}
+public class Mess {
+    public static void main(String[] args) {
+
+        int year = 2001;
+        String title = "Shrek";
+        char genre = 'C';
+        double runtime = 1.58;
+        boolean isPG = true;
+    }
+
 }
