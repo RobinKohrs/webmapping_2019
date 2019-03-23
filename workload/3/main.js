62702b4c02b34c7efde5281107ef6b2275ce62eb
RobinKohrs <robin.kohrs@gmx.de>
2019-03-23 20:35:31 +0100

    2/9 int

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,9 +1,15 @@
-public class Creator {
+//This is the class declaration
+public class CountComment {
+    //This is the main method that runs when you compile
     public static void main(String[] args) {
-        String name = "James Gosling";
-        int yearCreated = 1995;
+        //This is where you will define your variable
+        int numComments = 6;
+        //This is where you will print your variable
+        System.out.println(numComments);
 
-        System.out.println(name);
-        System.out.println(yearCreated);
     }
+
+    //This is the end of the class
 }
+
+//This is outside the class
