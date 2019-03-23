f9a534d892077489279a7d774dbfcbfb859e1edc
RobinKohrs <robin.kohrs@gmx.de>
2019-03-23 20:40:32 +0100

    3/9 doubles

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,15 +1,6 @@
-//This is the class declaration
-public class CountComment {
-    //This is the main method that runs when you compile
+public class MarketShare {
     public static void main(String[] args) {
-        //This is where you will define your variable
-        int numComments = 6;
-        //This is where you will print your variable
-        System.out.println(numComments);
-
+        double androidShare = 81.7;
+        System.out.println(androidShare);
     }
-
-    //This is the end of the class
 }
-
-//This is outside the class
