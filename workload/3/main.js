ec4edaab1c01386f4fd844e98529e0dc00bf5c5f
RobinKohrs <robin.kohrs@gmx.de>
2019-03-23 20:47:00 +0100

    5/9 char

--- workload/3/main.js
+++ workload/3/main.js
@@ -1,8 +1,6 @@
-public class Booleans {
-	public static void main(String[] args) { 
-    boolean intsCanHoldDecimals = false;
-
-    System.out.println(intsCanHoldDecimals);
-
+public class Char {
+	public static void main(String[] args) {   
+char expectedGrade = 'D';
+    System.out.println(expectedGrade);
 	}
 }
