84fbec0e6839dbc475d07f96c85c4459ab18f2b3
RobinKohrs <robin.kohrs@gmx.de>
2019-03-27 18:56:11 +0100

    27.3 Seminar

---
+++ adlerweg/main.js
@@ -0,0 +1,61 @@
+// alert("Hallo Welt");
+
+const div = document.getElementById("map")
+const breite1 = div.getAttribute("data-lat1");
+const laenge1 = div.getAttribute("data-lng1");
+const titel1 = div.getAttribute("data-title1");
+
+
+const breite2 = div.getAttribute("data-lat2");
+const laenge2 = div.getAttribute("data-lng2");
+const titel2 = div.getAttribute("data-title2");
+
+
+//console.log(breite,laenge,titel)
+
+// Karte initialisieren
+
+
+let karte = L.map("map");
+
+//console.log(karte);
+
+//aus Ausschnitt zoomen
+
+
+
+//openstreetmap hinzufügen
+L.tileLayer("http://{s}.tile.osm.org./{z}/{x}/{y}.png").addTo(karte);
+
+// Positionsmarker hinzufügen
+let pin1 = L.marker(
+    [breite1, laenge1]
+).addTo(karte);
+
+let pin2 = L.marker(
+    [breite2, laenge2]
+).addTo(karte);
+
+//Popup zum Pin hängen
+pin1.bindPopup(titel1).openPopup();
+pin2.bindPopup(titel2).openPopup();
+
+let markerGruppe = L.featureGroup().addTo(karte)  //Gruppe erzeugen!!
+
+for (let blick of ADLERBLICKE) { //Marker erzeugen und dann popup dranhängen
+    let blickpin = L.marker( //marker definieren
+        [blick.lat, blick.lng]
+    ).addTo(markerGruppe); // Marker in die Gruppe packen!!
+    blickpin.bindPopup(
+        `<h1>Standort ${blick.standort}</h1>
+                        <p>Höhe: ${blick.seehoehe}</p>
+                        <em>Kunde: ${blick.kunde}</em>`
+    );
+}
+
+console.log(blickeGruppe.getBounds())
+//Auf Adlerblicke zoomen
+karte.fitBounds (blickeGruppe.getBounds());
+
+
+
