00c3dd4f59a3a7e7ddff025302c7207a6a31e9de
unknown <robin.kohrs@gmx.de>
2019-05-08 18:57:44 +0200

    farpalette

--- awstirol/main.js
+++ awstirol/main.js
@@ -15,36 +15,108 @@ const kartenlayer = {
         subdomains: ["a", "b", "c"],
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
     }),
-    bmapgelaende : L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpeg", {
-  subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
-  attribution : 'Datenquelle: <a href="https://basemap.at“>basemap.at</a>'
-}),
+    bmapgelaende: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgelaende/grau/google3857/{z}/{y}/{x}.jpeg", {
+        subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+        attribution: 'Datenquelle: <a href="https://basemap.at“>basemap.at</a>'
+    }),
 }
 
 karte.addLayer(
     kartenlayer.bmapgelaende
 )
 
-L.control.layers({
+const layerControl = L.control.layers({
     "OpenStreetmap": kartenlayer.osm,
     "Stamen Toner": kartenlayer.stamen_toner,
     "Stamen Terrain": kartenlayer.stamen_terrain,
-    "Basemap Gelände" : kartenlayer.bmapgelaende,
+    "Basemap Gelände": kartenlayer.bmapgelaende,
 }).addTo(karte);
 
 karte.setView(
-    [47.267222 , 11.392778],
+    [47.267222, 11.392778],
     15
-    );
+);
 
 //console.log(AWS);
-const awsTIROL = L.featureGroup();
-L.geoJson(AWS)
-.bindPopup (function(layer) {
-    console.log("Layer: ", layer);
-    return `Temperatur: ${layer.feature.properties.LT} °C <br>
-    Datum: ${layer.feature.properties.date}`;
-})
-.addTo(awsTIROL); // erzeugt 168 Marker
-awsTIROL.addTo(karte); //Ich hänge sie nicht direkt an die Karte, sondern pack sie in die Featuregroup
-karte.fitBounds(awsTIROL.getBounds());
+
+async function loadStations() {
+    const response = await fetch("https://aws.openweb.cc/stations"); //await lässt einen darauf warten, dass der nächste Aufruf abschlossen wird
+    const stations = await response.json(); //Umwandeln --> Alle Daten die in AWS KONStante waren sind nun in Stations Konstante
+    const awsTIROL = L.featureGroup();
+    L.geoJson(stations)
+        .bindPopup(function (layer) {
+            //console.log("Layer: ", layer);
+            const date = new Date(layer.feature.properties.date);
+            console.log("Datum: ", date);
+            return `<h4>${layer.feature.properties.name}</h4>
+            Höhe (m): ${layer.feature.geometry.coordinates[2]} <br>
+            Temperatur: ${layer.feature.properties.LT} °C <br>
+    Datum: ${date.toLocaleDateString("de-AT")}
+           ${date.toLocaleTimeString("de-AT")} <br>
+           Windgeschwindigkeit (km/h): 
+           ${layer.feature.properties.WG ? layer.feature.properties.WG : `keine Daten`}
+           <hr>
+           <footer> Land Tirol - <a href ="data.tirol.gv.at"> data.tirol.gv.at</a></footer> 
+           `;
+        })
+        .addTo(awsTIROL); // erzeugt 168 Marker
+    //Ich hänge sie nicht direkt an die Karte, sondern pack sie in die Featuregroup
+    karte.fitBounds(awsTIROL.getBounds());
+    layerControl.addOverlay(awsTIROL, "wetterstationen tirol");
+
+
+    //Windrichtungen anzeigen
+    const windlayer = L.featureGroup(); //Feature-Group erstellen 
+    L.geoJson(stations, { //neuer GeoJson-Layer
+        pointToLayer: function (feature, latlng) { //für jeden Punkt
+            if (feature.properties.WR) {
+                let color = 'black';
+                if (feature.properties.WG > 20) { // Pfeil ist normalerweise black, wenn > 20 wird sie rot
+                    color = 'red';
+                }
+                return L.marker(latlng, {
+                    icon: L.divIcon({
+                        html: `<i style="color: ${color}; transform: rotate(${feature.properties.WR}deg)" class="fas fa-arrow-circle-up fa-2x"></i>`
+                    })
+                });
+            }
+        }
+    }).addTo(windlayer); //hänge das ganze an meinen windlayer
+    layerControl.addOverlay(windlayer, "Windrichtung")
+    //windlayer.addTo(karte) //wird direkt angezeift
+
+    // Temperaturlayer anzeigen
+    const temperaturLayer = L.featureGroup(); //Feature-Group erstellen 
+    const farbPalette = [
+        [0, "blue"],
+        [1, "yellow"],
+        [2, "orange"],
+        [5, "red"],
+    ];
+
+    L.geoJson(stations, { //neuer GeoJson-Layer
+        pointToLayer: function (feature, latlng) { //für jeden Punkt
+            if (feature.properties.LT) {
+                let color = 'red';
+                for (let i=0; i<farbPalette.length; i++) {
+                    console.log(farbPalette[i], feature.properties.LT);
+                    if (feature.properties.LT < farbPalette[i][0]) {
+                        color = farbPalette[i][1];
+                        break;
+                    }
+                }
+
+                return L.marker(latlng, {
+                    icon: L.divIcon({
+                        html: `<div class="temperaturLabel" style="background-color:${color}">${feature.properties.LT}</div>`
+                    })
+                });
+            }
+        }
+    }).addTo(temperaturLayer); //hängt das ganze an meinen templayer
+    layerControl.addOverlay(temperaturLayer, "temperatur")
+    temperaturLayer.addTo(karte) //wird direkt angezeift
+};
+
+
+loadStations();
