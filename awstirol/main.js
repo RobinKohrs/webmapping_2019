b67aef7d2a1f68851bc8f2d8c0c6f5f9f3c6a957
unknown <robin.kohrs@gmx.de>
2019-05-13 17:20:47 +0200

    fixed array

--- awstirol/main.js
+++ awstirol/main.js
@@ -40,15 +40,15 @@ karte.setView(
 //console.log(AWS);
 
 async function loadStations() {
-    const response = await fetch("https://aws.openweb.cc/stations"); //await lässt einen darauf warten, dass der nächste Aufruf abschlossen wird
-    const stations = await response.json(); //Umwandeln --> Alle Daten die in AWS KONStante waren sind nun in Stations Konstante
-    const awsTIROL = L.featureGroup();
-    L.geoJson(stations)
-        .bindPopup(function (layer) {
-            //console.log("Layer: ", layer);
-            const date = new Date(layer.feature.properties.date);
-            console.log("Datum: ", date);
-            return `<h4>${layer.feature.properties.name}</h4>
+        const response = await fetch("https://aws.openweb.cc/stations"); //await lässt einen darauf warten, dass der nächste Aufruf abschlossen wird
+        const stations = await response.json(); //Umwandeln --> Alle Daten die in AWS KONStante waren sind nun in Stations Konstante
+        const awsTIROL = L.featureGroup();
+        L.geoJson(stations)
+            .bindPopup(function (layer) {
+                //console.log("Layer: ", layer);
+                const date = new Date(layer.feature.properties.date);
+                console.log("Datum: ", date);
+                return `<h4>${layer.feature.properties.name}</h4>
             Höhe (m): ${layer.feature.geometry.coordinates[2]} <br>
             Temperatur: ${layer.feature.properties.LT} °C <br>
     Datum: ${date.toLocaleDateString("de-AT")}
@@ -58,80 +58,87 @@ async function loadStations() {
            <hr>
            <footer> Land Tirol - <a href ="data.tirol.gv.at"> data.tirol.gv.at</a></footer> 
            `;
-        })
-        .addTo(awsTIROL); // erzeugt 168 Marker
-    //Ich hänge sie nicht direkt an die Karte, sondern pack sie in die Featuregroup
-    karte.fitBounds(awsTIROL.getBounds());
-    layerControl.addOverlay(awsTIROL, "wetterstationen tirol");
+            })
+            .addTo(awsTIROL); // erzeugt 168 Marker
+        //Ich hänge sie nicht direkt an die Karte, sondern pack sie in die Featuregroup
+        karte.fitBounds(awsTIROL.getBounds());
+        layerControl.addOverlay(awsTIROL, "wetterstationen tirol");
 
 
-    //Windrichtungen anzeigen
-    const windlayer = L.featureGroup(); //Feature-Group erstellen 
-    L.geoJson(stations, { //neuer GeoJson-Layer
-        pointToLayer: function (feature, latlng) { //für jeden Punkt
-            if (feature.properties.WR) {
-                let color = 'black';
-                if (feature.properties.WR > 20) { // Pfeil ist normalerweise black, wenn > 20 wird sie rot
-                    color = 'red';
+        //Windrichtungen anzeigen
+        const windlayer = L.featureGroup(); //Feature-Group erstellen 
+        L.geoJson(stations, { //neuer GeoJson-Layer
+            pointToLayer: function (feature, latlng) { //für jeden Punkt
+                if (feature.properties.WR) {
+                    let color = 'black';
+                    if (feature.properties.WR > 20) { // Pfeil ist normalerweise black, wenn > 20 wird sie rot
+                        color = 'red';
+                    }
+                    return L.marker(latlng, {
+                        icon: L.divIcon({
+                            html: `<i style="color: ${color}; transform: rotate(${feature.properties.WR}deg)" class="fas fa-arrow-circle-up fa-2x"></i>`
+                        })
+                    });
                 }
-                return L.marker(latlng, {
-                    icon: L.divIcon({
-                        html: `<i style="color: ${color}; transform: rotate(${feature.properties.WR}deg)" class="fas fa-arrow-circle-up fa-2x"></i>`
-                    })
-                });
             }
-        }
-    }).addTo(windlayer); //hänge das ganze an meinen windlayer
-    layerControl.addOverlay(windlayer, "Windrichtung")
-    //windlayer.addTo(karte) //wird direkt angezeift
+        }).addTo(windlayer); //hänge das ganze an meinen windlayer
+        layerControl.addOverlay(windlayer, "Windrichtung")
+        //windlayer.addTo(karte) //wird direkt angezeift
 
-    // Temperaturlayer anzeigen
-    const temperaturLayer = L.featureGroup(); //Feature-Group erstellen 
-    const farbPalette = [
-        [0, "blue"],
-        [1, "yellow"],
-        [2, "orange"],
-        [5, "red"],
-    ];
+        // Temperaturlayer anzeigen
+        const temperaturLayer = L.featureGroup(); //Feature-Group erstellen 
+        let farbPalette = [
+            [0, "blue"],
+            [1, "yellow"],
+            [2, "orange"],
+            [5, "red"],
+        ];
 
-    L.geoJson(stations, { //neuer GeoJson-Layer
-        pointToLayer: function (feature, latlng) { //für jeden Punkt
-            if (feature.properties.LT) {
-                let color = 'red';
-                for (let i=0; i<farbPalette.length; i++) {
-                    console.log(farbPalette[i], feature.properties.LT);
-                    if (feature.properties.LT < farbPalette[i][0]) {
-                        color = farbPalette[i][1];
-                        break;
+        L.geoJson(stations, { //neuer GeoJson-Layer
+            pointToLayer: function (feature, latlng) { //für jeden Punkt
+                if (feature.properties.LT) {
+                    let color = farbPalette[farbPalette.length - 1][1]; //length =10
+                    for (let i = 0; i < farbPalette.length; i++) {
+                        console.log(farbPalette[i], feature.properties.LT);
+                        if (feature.properties.LT < farbPalette[i][0]) {
+                            color = farbPalette[i][1];
+                            break;
+                        }
+                    }
+
+                    return L.marker(latlng, {
+                        icon: L.divIcon({
+                            html: `<div class="temperaturLabel" style="background-color:${color}">${feature.properties.LT}</div>`
+                        })
+                    });
+                }
+            }
+        }).addTo(temperaturLayer); //hängt das ganze an meinen templayer
+        layerControl.addOverlay(temperaturLayer, "temperatur")
+        temperaturLayer.addTo(karte) //wird direkt angezeift
+
+
+
+
+        // relative Feuchte
+        const relfeuchte = L.featureGroup()
+        farbPalette = [
+            [30, "#EEE"],
+            [40, "#DDD"], // 1. SChwelle 2. Farbe
+
+        ]
+
+        L.geoJson(stations, {
+                pointToLayer: function (feature, latlng) {
+                    if (feature.properties.RH) {
+                        let color = farbPalette[farbPalette.length - 1][1];
+                        for (let i = 0; i < farbPalette; i++) {
+
+                        }
                     }
                 }
 
-                return L.marker(latlng, {
-                    icon: L.divIcon({
-                        html: `<div class="temperaturLabel" style="background-color:${color}">${feature.properties.LT}</div>`
-                    })
-                });
-            }
-        }
-    }).addTo(temperaturLayer); //hängt das ganze an meinen templayer
-    layerControl.addOverlay(temperaturLayer, "temperatur")
-    temperaturLayer.addTo(karte) //wird direkt angezeift
-};
+        })
 
 
-
-// relative Feuchte
-const relfeuchte = L.featureGroup()
-const = farbPaletterh [
-]
-
-L.geoJson(stations, {
-    pointToLayer: function (feature, latlng) {
-    if (feature.properties.RH) {
-        let color = "#EEE";
-        for (let i=0, i<farbPaletterh, i++) {
-    }
-})
-
-
-loadStations();
+        loadStations();
